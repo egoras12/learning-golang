dc697836b4ae24019fe44da3a6682eb430a95270
Bethrand Nnaemeka <bethrand2019@gmail.com>
2026-01-24 22:55:10 +0100

    [DOCS]: Add explanation for pass by value behavior in comments

--- main.go
+++ main.go
@@ -17,3 +17,5 @@ func main() {
 
 	fmt.Println(name)
 }
+
+// Well still trying to understand this pass by value thing, but from what I get is that when we pass a variable to a function, it creates a copy of that variable and any changes made to that variable inside the function do not affect the original variable outside the function. So in this case, when we call updateName(name), it creates a copy of name and assigns it to x. When we change x to "Khalifa", it does not change the original name variable in main, which remains "Mia". Hence, when we print name in main, it still outputs "Mia".
