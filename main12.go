82cf62ff5dff587932edf55ff14374f69bd0d3fd
Bethrand Nnaemeka <bethrand2019@gmail.com>
2026-03-30 18:41:11 +0100

    [FEAT]: Implement main12 function with struct discussion and constant declaration for pi

---
+++ main12.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main12() {
+	fmt.Println("This is the next file on the line, and we're going to talk about struct in this file")
+	fmt.Println("when running this one, input thi command ==> go run main12.go bill12.go. because 2 files are dependent on them")
+
+
+	bill := bill12("Bethrand")
+	bill13 := bill12("Beth") // this is a redeclaration of the bill variable, and it will cause an error. because we can't redeclare a variable in the same scope. to fix this, we can either change the name of the variable or we can use the short declaration operator (:=) to declare and initialize the variable in one step. like this: bill := bill12("Beth")
+
+	fmt.Println(bill)
+	fmt.Println(bill13)
+
+	// Well, I learn about const today 
+
+	const pi = 3.14 // this is a constant variable, and it cannot be changed. if we try to change the value of pi, it will cause an error. because constants are immutable, and they cannot be changed after they are declared.
+}
