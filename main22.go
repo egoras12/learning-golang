f8fb96f3c8ed3ec64978b8bc5ff37b4a069e168e
Bethrand Nnaemeka <bethrand2019@gmail.com>
2026-05-08 19:18:41 +0100

    [FEAT]: Add main22.go with authenticationInfo struct and login method for user authorization

---
+++ main22.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type authenticationInfo struct {
+	username string
+	password string
+}
+
+// Let's clean up Textio's authentication logic. We store our user's authentication data inside an authenticationInfo struct. We need a method that can take that data and return a basic authorization string.
+
+func (auth authenticationInfo) login() string {
+	return fmt.Sprintf("Authorization: Basic %v:%v", auth.username, auth.password)
+}
+
+func main22() {
+
+	person := authenticationInfo{
+		"Bethrand",
+		"1234567890",
+	}
+
+	fmt.Println(person.login())
+}
