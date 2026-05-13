7efb4697661b618170d7b3f41d862edf42685020
Bethrand Nnaemeka <bethrand2019@gmail.com>
2026-05-13 18:24:19 +0100

    [REFAC]: Clean up main.go by removing commented-out code and consolidating user message logic

--- main.go
+++ main.go
@@ -1,97 +1,14 @@
-// package main                      // first implementation, before refactoring
-
-// import "fmt"
-
-// type User struct {
-// 	Name string
-// 	Membership
-// }
-
-// type Membership struct {
-// 	Type string
-// 	MessageCharLimit int
-// }
-
-// func newUser(name string, membershipType string) User {
-// 	// ?
-
-// 	if membershipType == "premium" {
-// 		return User{
-// 			Name: name,
-// 			Membership: Membership{
-// 				Type: "premium",
-// 				MessageCharLimit: 1000,
-// 			},
-// 		}
-// 	} else {
-// 		return User{
-// 			Name: name,
-// 			Membership: Membership{
-// 				Type: "regular",
-// 				MessageCharLimit: 100,
-// 			},
-// 		}
-// 	}
-// }
-
-// func main24 () {
-// 	fmt.Println(newUser("Bethrand", "premium"))
-// }
-
 package main
 
 import "fmt"
 
-//
-
-func (u User) sendMessage(message string, messageLength int) (string, bool) {
-
-	if messageLength > u.MessageCharLimit {
-		return "", false
-	}
-
-	return message, true
-}
-
-// don't touch below this line
-
-type User struct {
-	Name string
-	Membership
-}
-
-type Membership struct {
-	Type             string
-	MessageCharLimit int
-}
-
-func newUser(name string, membershipType string) User {
-	membership := Membership{Type: membershipType}
-	if membershipType == "premium" {
-		membership.MessageCharLimit = 1000
-	} else {
-		membership.Type = "standard"
-		membership.MessageCharLimit = 100
-	}
-	return User{Name: name, Membership: membership}
-}
-
-func main() {
-
-	// first implementation, before refactoring
-
-	// newUserMember := User{
-	// 	"Bethrand",
-	// 	Membership{
-	// 		Type:             "premium",
-	// 		MessageCharLimit: 1000,
-	// 	},
-	// }
-
-	// fmt.Println(newUserMember.sendMessage("Hello, World!", len("Hello, World!")))
-
+func main () {
 	user := newUser("Bethrand", "premium")
+	message, sent := user.sendMessage("Hello, World!", 13)
 
-	msg, ok := user.sendMessage("Hello World", len("Hello World!"))
-	fmt.Println(msg, ok)
+	if sent {
+		fmt.Printf("Message sent: %s\n", message)
+	} else {
+		fmt.Println("Message not sent: exceeds character limit.")
+	}
 }
