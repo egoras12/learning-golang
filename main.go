34f227c753354c1f49d5dcbda615aef0f5581987
Bethrand Nnaemeka <bethrand2019@gmail.com>
2026-05-23 19:20:38 +0100

    [FIX]: Correct typo in message output for consistency

--- main.go
+++ main.go
@@ -4,5 +4,5 @@ import ("fmt")
 
 func main() {
 	fmt.Println("We're going to be learning about interfaces in Go!")
-	fmt.Println("Haven't done anything in GO in a long while")
+	fmt.Println("Haven't done anything in GO in a long long while")
 }
